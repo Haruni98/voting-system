89552e1495daf91db5f11311794b26a67919e26c
Haruni98 <lonahnaftal@gmail.com>
2025-11-08 15:29:23 +0300

    Firt commit (started to create the structure)

---
+++ voting_system.js
@@ -0,0 +1,7 @@
+function showHB_info(){
+    var x =document.getElementById('contestants_info_container');
+    x.style.visibility = 'visible';
+    var y =document.getElementById('whoAmI_HB');
+    y.style.color ='white';
+    y.style.backgroundColor ='brown';
+}
